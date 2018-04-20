990e4d40bd94d40837e3e3fca70b1b8f5947776a
unknown <dldhwn56@gmail.com>
2018-04-20 15:45:03 +0900

    1번

--- src/main/java/kr/ac/jejunu/ProductDao.java
+++ src/main/java/kr/ac/jejunu/ProductDao.java
@@ -4,25 +4,62 @@ import java.sql.*;
 
 public class ProductDao {
     public Product get(Long id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
+        Connection connection = getConnection();
 
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from product where id = ?");
-        preparedStatement.setLong(1, id);
-
+        //preparedStatement
+        PreparedStatement preparedStatement =
+                connection.prepareStatement(
+                        "SELECT * FROM product WHERE id = ?");
+        preparedStatement.setLong(1,id);
+        //resultset
         ResultSet resultSet = preparedStatement.executeQuery();
         resultSet.next();
-
+        //product mapping
         Product product = new Product();
         product.setId(resultSet.getLong("id"));
         product.setTitle(resultSet.getString("title"));
         product.setPrice(resultSet.getInt("price"));
-
-        //자원을 해지한다.
+        //close
         resultSet.close();
         preparedStatement.close();
         connection.close();
-
+        //return
         return product;
     }
+
+    public long insert(Product product) throws SQLException, ClassNotFoundException {
+        Connection connection = getConnection();
+
+        //preparedStatement
+        PreparedStatement preparedStatement =
+                connection.prepareStatement(
+                        "INSERT INTO product(title, price) VALUES (?,?)");
+        preparedStatement.setString(1, product.getTitle());
+        preparedStatement.setInt(2, product.getPrice());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement = connection.prepareStatement("SELECT last_insert_id()");
+
+        //resultset
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        //id mapping
+        Long id = resultSet.getLong(1);
+        //close
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        //return
+        return id;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        //connection
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost/midprac", "ojlee", "123456");
+    }
+
 }
--- src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -1,5 +1,6 @@
 package kr.ac.jejunu;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.SQLException;
@@ -7,12 +8,15 @@ import java.sql.SQLException;
 import static org.junit.Assert.assertEquals;
 
 public class ProductDaoTest {
-    public void setup() {
+
+    private ProductDao productDao;
+    @Before
+    public void setup(){
+        productDao = new ProductDao();
     }
 
     @Test
     public void get() throws SQLException, ClassNotFoundException {
-        ProductDao productDao = new ProductDao();
         Long id = 1L;
         String title = "제주감귤";
         Integer price = 15000;
@@ -21,5 +25,18 @@ public class ProductDaoTest {
         assertEquals(id, product.getId());
         assertEquals(title, product.getTitle());
         assertEquals(price, product.getPrice());
+    }
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        Product product = new Product();
+        product.setTitle("다금바리");
+        product.setPrice(200000);
+        Long id = productDao.insert(product);
+
+        Product insertedProduct = productDao.get(id);
+        assertEquals(insertedProduct.getId(), id);
+        assertEquals(insertedProduct.getTitle(), product.getTitle());
+        assertEquals(insertedProduct.getPrice(), product.getPrice());
+
     }
 }
